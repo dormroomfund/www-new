5cddfe9b5200155604c1421906c3f66392360ab0
Maneet Khaira <msk2226@columbia.edu>
2017-07-22 00:04:34 -0700

    Added logo and menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import drf_logo_black from './img/drf-logo-black.svg'
 import './css/App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+              <Menu />
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -21,12 +24,10 @@ class App extends Component {
 class Menu extends Component {
     render() {
         return (
-            <div class="content">
-              <div class="col-12">
-                  <div class="drf-logo-black">
-                      <a href="/" class="close-contact-modal-link"></a>
-                  </div>
-                  <ul class="links">
+            <div className="content">
+              <div className="col-12">
+                  <img alt="Dorm Room Fund logo" className="drf-logo-black" src={drf_logo_black}></img>
+                  <ul className="links">
                       <li>
                           <a href="/founders/">Our Founders</a>
                       </li>
