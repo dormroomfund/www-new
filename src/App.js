852891849965c2752644c995debb0bdad8bd5557
Maneet Khaira <msk2226@columbia.edu>
2017-07-22 13:57:32 -0700

    Added ramp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import drf_logo_black from './img/drf-logo-black.svg'
+import ramp from './img/ramp.svg'
+
 import './css/App.css';
 
 class App extends Component {
@@ -45,8 +46,14 @@ class Hero extends Component {
   render(){
     return(
       <div>
-                
       <Menu/>
+      <div className="content">
+      <div className="hero-wrapper">
+      <h1 className="hero-header">Hi. We’re Dorm Room Fund, the best way for student founders to accelerate their startup.</h1>
+      <p className="hero-subheader">Our community provides founders a stong network of other founders and VCs, access to dozens of mentors, business development deals, and $20,000.</p>
+      </div>
+      </div>
+      <img className="ramp" src={ramp}></img>
       </div>
     )
   }
