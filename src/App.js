d5026acb2f2261a94faa96ad9c25bad8cf10874a
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-26 12:30:45 -0700

    Comment out missing image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import investor_firstround from './img/investor-firstround.svg'
 import investor_gv from './img/investor-gv.png'
 import investor_sequoia from './img/investor-sequoia.svg'
 import investor_yc from './img/investor-yc.svg'
-import community_photo from './img/community-photo.png'
+// import community_photo from './img/community-photo.png'
 
 import './css/App.css';
 
@@ -57,7 +57,7 @@ class Community extends Component {
     <p className="community-subheader light-text">Our community provides founders a stong network of other founders and VCs, access to dozens of mentors, business development deals, and $20,000. Our community provides.</p>
     <a className="our-team-button button-rounded-white">Our Team</a>
     </div>
-    <img className="community-image" src={community_photo}></img>
+    {/* <img className="community-image" src={community_photo}></img> */}
     </div>
     </div>
     )
