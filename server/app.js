ce3f1bc4fc6164406d19b1db33c556a2e4a0fbd2
Maneet Khaira <msk2226@columbia.edu>
2017-08-28 20:15:36 -0700

    Comment

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const app = express();
 // Setup logger
 // app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
+
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
