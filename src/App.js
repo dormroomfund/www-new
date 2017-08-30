9af87191e370dc5233aa8fa6e3f1cb613b16a531
Maneet Khaira <msk2226@columbia.edu>
2017-08-30 02:33:28 -0700

    Fix typo & update bios

--- src/App.js
+++ src/App.js
@@ -399,7 +399,7 @@ class Value extends Component {
     return(
       <div className="value-section">
       <div className="content">
-      <h2>So, what do Dorm Room Fund founders recieve?</h2>
+      <h2>So, what do Dorm Room Fund founders receive?</h2>
       <p className="value-section-subheader">We're designed to help student founders go from the earliest stage to the seed stage and beyond.</p>
       <div className="value-section-cards content">
       <div className="col-4 m-col-12"><div className="value-section-card"><h3>Community</h3><p className="value-section-text">300+ fellow founders, 50+ mentors, dozens of intimate events with industry leaders and investors.</p></div></div>
