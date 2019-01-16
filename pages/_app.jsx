ed595d9edfa025fb9ab63564525f3819c8e17b3b
Casey Chow <casey.chow@gmail.com>
2019-01-16 13:53:29 -0500

    better rendering

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -33,16 +33,6 @@ export default class MyApp extends App {
 
     return (
       <Container>
-        <Head>
-          <style type="text/css">{`
-          html {
-            display: none;
-          }
-          html.wf-active {
-            display: block;
-          }
-        `}</style>
-        </Head>
         <Component {...pageProps} />
       </Container>
     );
