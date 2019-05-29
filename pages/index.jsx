00a5d14dcee930eeb6fb3bc0d43591a0aff0aadd
Vinai Rachakonda <rachakondavinai@gmail.com>
2019-05-29 15:43:43 -0400

    Update to dynamically grab medium posts. Remove .idea tracking and us… (#38)
    * Update to dynamically grab medium posts. Remove .idea tracking and usless article images
    
    * Switch getInitialProps()
    
    * Add comment to error
    
    * Forgot to pass articles as a function parameter

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,8 @@
+import { Component } from 'react';
+
 import Head from 'next/head';
 import Apply from '../components/Apply';
+import Banner from '../components/Banner';
 import Resources from '../components/Resources';
 import CustomFooter from '../components/Footer';
 import Menu from '../components/Menu';
@@ -7,21 +10,41 @@ import Hero from '../components/Hero';
 import Community from '../components/Community';
 import Value from '../components/Value';
 import FoundersSection from '../components/FoundersSection';
-import Banner from '../components/Banner';
+import axios from 'axios';
 
-export default () => (
-  <div className="App">
-    <Head>
-      <title>Dorm Room Fund</title>
-    </Head>
-    <Banner />
-    <Menu lightColor={false} />
-    <Hero />
-    <Community />
-    <Value />
-    <FoundersSection />
-    <Apply />
-    <Resources />
-    <CustomFooter />
-  </div>
-);
+export default class Index extends Component {
+  static async getInitialProps() {
+    try {
+      const mediumRssFeed = 'https://medium.com/feed/@dormroomfund';
+      const rssToJsonApi = 'https://api.rss2json.com/v1/api.json';
+      const data = { params: { rss_url: mediumRssFeed } };
+      const articles = (await axios.get(rssToJsonApi, data)).data.items.slice(
+        0,
+        3
+      ); // take first 3 articles
+      return { articles };
+    } catch (error) {
+      console.log(`Medium article loading error: ${error}`);
+      return { articles: [] };
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Head>
+          <title>Dorm Room Fund</title>
+        </Head>
+        <Banner />
+        <Menu lightColor={false} />
+        <Hero />
+        <Community />
+        <Value />
+        <FoundersSection />
+        <Apply />
+        <Resources articles={this.props.articles} />
+        <CustomFooter />
+      </div>
+    );
+  }
+}
