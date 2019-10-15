f0f28534138893d914250523de094bb46ac9625b
ERHSQuizbowl <vinaichand@yahoo.com>
2019-10-15 18:37:24 -0400

    Update navbar with jobboard

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -157,6 +157,15 @@ export default class Menu extends Component {
                   </a>
                 </li>
                 <li>
+                  <a
+                    className={'menu ' + (this.props.lightColor ? 'light' : '')}
+                    target="_blank"
+                    href="https://www.notion.so/dormroomfund/528f475499b2424ea9839ca87344c804?v=c522c87778014443a79ffcb99ec15013"
+                  >
+                    Jobs
+                  </a>
+                </li>
+                <li>
                   <Link href="/apply">
                     <a
                       className={
