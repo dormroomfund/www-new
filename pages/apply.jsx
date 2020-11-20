fa8066529bab5d29f3a82b45c109f31001f48b46
Anish Agnihotri <contact@anishagnihotri.com>
2020-11-20 00:19:23 -0500

    Application form to Airtable

--- pages/apply.jsx
+++ pages/apply.jsx
@@ -1,22 +1,28 @@
 import { Component } from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
-
-// I,port component with no SSR
-const ReactTypeformEmbed = dynamic(
-  import('react-typeform-embed').then((mod) => mod.ReactTypeformEmbed),
-  { ssr: false }
-);
-
 export default class ApplyPage extends Component {
   render() {
     return (
       <div>
         <Head>
           <title>Dorm Room Fund - Apply</title>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `https://static.airtable.com/js/embed/embed_snippet_v1.js`,
+            }}
+          />
         </Head>
-        <ReactTypeformEmbed
-          url={'https://dormroomfund.typeform.com/to/H90ZNU'}
+        <iframe
+          className="airtable-embed airtable-dynamic-height"
+          src="https://airtable.com/embed/shrIkIQTZqkdcS05v?backgroundColor=blue"
+          frameBorder="0"
+          onMouseWheel=""
+          width="100%"
+          height="4826"
+          style={{
+            background: 'transparent',
+          }}
         />
       </div>
     );
